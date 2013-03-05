0210f671b23f790058401baca18fb47701c5caa1
kevin <huangtao@antiy.com>
2013-03-05 17:36:29 +0800

    procedure.txt

--- node-nxweb/src/include/nxweb/config.h
+++ node-nxweb/src/include/nxweb/config.h
@@ -42,19 +42,19 @@
 #define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#define PACKAGE "indexserver"
+#define PACKAGE "nodeserver"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "indexserver"
+#define PACKAGE_NAME "nodeserver"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "indexserver 3.0.1"
+#define PACKAGE_STRING "nodeserver 3.0.1"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "indexserver"
+#define PACKAGE_TARNAME "nodeserver"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
