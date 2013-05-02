5429c834a0d8e0be2aeb80a8018d286b4c4e66fe
kevin <huangtao@antiy.com>
2013-05-02 09:55:37 +0800

    add if_status.c in node.

---
+++ src/node/if_status.c
@@ -0,0 +1,23 @@
+/*
+ * huangtao@antiy.com
+ */
+
+#include "nxweb/nxweb.h"
+#include <stdio.h>
+#include <string.h>
+#include "an.h"
+
+static nxweb_result status_on_request(
+	nxweb_http_server_connection* conn, 
+	nxweb_http_request* req, 
+	nxweb_http_response* resp) 
+{
+    nxweb_response_printf(resp, "OK. Alive");
+    return NXWEB_OK;
+}
+
+nxweb_handler status_handler={
+    .on_request = status_on_request,
+    .flags = NXWEB_PARSE_PARAMETERS
+};
+
