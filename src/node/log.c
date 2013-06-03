1979be7f1734d555856ae3d44700f88d13c31afb
kevin <huangtao@antiy.com>
2013-06-03 16:52:22 +0800

    common

--- src/node/log.c
+++ src/node/log.c
@@ -17,10 +17,10 @@ static FILE *f_log = NULL;
 static pthread_mutex_t mutex;
 static char instance_id[64] = {0};
 
+
 int log_init(const char *filename)
 {
-    if (strlen(filename) > 256)
-	return -1;
+    if (strlen(filename) > 256) {return -1;}
 
     time_t t = time(NULL);
     struct tm *lt = NULL;
@@ -31,8 +31,7 @@ int log_init(const char *filename)
 
     strncpy(log_file, filename, sizeof(log_file));
     f_log = fopen(log_file, "a");
-    if (f_log == NULL)
-	return -1;
+    if (f_log == NULL) {return -1;}
     fprintf(f_log, "\n");
     //fprintf(f_log, "TIME\t\t\tID\tLEVEL\tMODULE\tINFORMATION\n");
     fflush(f_log);
@@ -54,39 +53,27 @@ void log_release()
 
 void log_out(const char *module, const char *info, LOG_LEVEL level)
 {
+    if (level < 0 || level > 5) {return ;}
+    else if (level > g_log_level) {return ;}
+    if (pthread_mutex_lock(&mutex) != 0) {return ;}
+
     time_t t = time(NULL);
     struct tm *lt;
     char stime[32] = {0};
 
-    if (level < 0 || level > 5)
-	return ;
-    else if (level > g_log_level)
-	return ;
-
     lt = localtime(&t);
     strftime(stime, sizeof(stime), "%Y-%m-%d %H:%M:%S", lt);
-
-    if (pthread_mutex_lock(&mutex) != 0)
-	return ;
-
-    if (f_log == NULL)
-	f_log = stdout;
+    if (f_log == NULL) {f_log = stdout;}
 
     fprintf(f_log, "%s\t", stime);
     fprintf(f_log, "%s\t", instance_id);
     switch (level) {
-	case 0:
-	    fprintf(f_log, "L0-system\t"); break;
-	case 1:
-	    fprintf(f_log, "L1--fatal\t"); break;
-	case 2:
-	    fprintf(f_log, "L2--error\t"); break;
-	case 3:
-	    fprintf(f_log, "L3---warn\t"); break;
-	case 4:
-	    fprintf(f_log, "L4---info\t"); break;
-	case 5:
-	    fprintf(f_log, "L5--debug\t"); break;
+	case 0: fprintf(f_log, "L0-system\t"); break;
+	case 1: fprintf(f_log, "L1--fatal\t"); break;
+	case 2: fprintf(f_log, "L2--error\t"); break;
+	case 3: fprintf(f_log, "L3---warn\t"); break;
+	case 4: fprintf(f_log, "L4---info\t"); break;
+	case 5: fprintf(f_log, "L5--debug\t"); break;
     }
     fprintf(f_log, "M-%s\t", module);
     fprintf(f_log, "%s\t", info);
